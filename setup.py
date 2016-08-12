d7cb95d703e85b9298c71cd05377ab1f0808f36f
Alex Shafer <ashafer01@gmail.com>
2016-08-12 16:37:21 -0700

    adding laurelin namespace package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='laurelin-ldap',
+    version='0.1',
+    namespace_packages=['laurelin']
+    packages=find_packages(),
+    install_requires=[
+        'pyasn1',
+    ],
+)
