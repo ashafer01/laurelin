00f00f12454f14c63b031e2c6f53c1c34c2a1251
Alex Shafer <ashafer01@gmail.com>
2018-02-20 08:19:46 -0800

    various pre-deployment updates

--- setup.py
+++ setup.py
@@ -4,11 +4,39 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     install_requires = f.read().split()
 
+description = 'A pure-Python ORM-esque LDAP client.'
+
 setup(
     name='laurelin-ldap',
     version='0.1',
+    description=description,
+    long_description=description,
     author='Alex Shafer',
     author_email='ashafer01@gmail.com',
+    url='https://github.com/ashafer01/laurelin',
+    license='LGPLv3+',
+    keywords='ldap',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+    ],
     namespace_packages=['laurelin', 'laurelin.extensions'],
     packages=find_packages(),
     install_requires=install_requires,
