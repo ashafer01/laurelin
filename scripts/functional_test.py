a967c5c0376fc022a2b19c378bbc868fd360e472
Alex Shafer <ashafer01@gmail.com>
2018-03-15 13:26:17 -0700

    trying automated functional test with docker

--- scripts/functional_test.py
+++ scripts/functional_test.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-from getpass import getpass
 from laurelin.ldap import LDAP, LDAPObject
 from laurelin.ldap.schema import SchemaValidator
 
 LDAP.enable_logging()
 LDAP.activate_extension('laurelin.extensions.descattrs')
-with LDAP('ldap://localhost:10389',
-          validators=[SchemaValidator()],
-          ) as ldap:
+with LDAP('ldap://localhost:10389', validators=[SchemaValidator()]) as ldap:
     ldap.start_tls(verify=False)
-    ldap.simple_bind(username='cn=admin,dc=example,dc=org', password=getpass())
+    ldap.simple_bind(username='cn=admin,dc=example,dc=org', password='admin')
 
     print(ldap.root_dse.format_ldif())
 
