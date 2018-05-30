c695b2a8f42c9b3d727fdb8023927c190a8bc5c1
Alex Shafer <ashafer01@gmail.com>
2018-05-30 00:06:43 -0700

    break out AVA parsing

--- laurelin/ldap/filter.py
+++ laurelin/ldap/filter.py
@@ -55,7 +55,7 @@ def escape(text):
 
 
 def parse(filter_str):
-    """Parse a filter string to a protocol-level object"""
+    """Parse an RFC 4515 filter string to a protocol-level object"""
 
     fil = Filter()
     try:
@@ -71,105 +71,7 @@ def parse(filter_str):
         not_filter.setComponentByName('innerNotFilter', parse(chunk[1:]))
         fil.setComponentByName('not', not_filter)
     else:
-        try:
-            attr, val = chunk.split('=', 1)
-        except ValueError:
-            raise LDAPError('Invalid filter - missing =')
-        if attr[-1] == '>':
-            ava = GreaterOrEqual()
-            ava.setComponentByName('attributeDesc', AttributeDescription(attr[0:-1].strip()))
-            ava.setComponentByName('assertionValue', AssertionValue(val))
-            fil.setComponentByName('greaterOrEqual', ava)
-        elif attr[-1] == '<':
-            ava = LessOrEqual()
-            ava.setComponentByName('attributeDesc', AttributeDescription(attr[0:-1].strip()))
-            ava.setComponentByName('assertionValue', AssertionValue(val))
-            fil.setComponentByName('lessOrEqual', ava)
-        elif attr[-1] == '~':
-            ava = ApproxMatch()
-            ava.setComponentByName('attributeDesc', AttributeDescription(attr[0:-1].strip()))
-            ava.setComponentByName('assertionValue', AssertionValue(val))
-            fil.setComponentByName('approxMatch', ava)
-        elif attr[-1] == ':':
-            # 1
-            # attr:=value
-            # 2
-            # attr:dn:=value
-            # attr:rule:=value
-            # :rule:=value
-            # 3
-            # attr:dn:rule:=value
-            # :dn:rule:=value
-
-            params = attr[0:-1].split(':')
-            n = len(params)
-
-            dnattrs = False
-            rule = None
-
-            if n == 1:
-                attr = params[0]
-            elif n == 2:
-                attr = params[0]
-                if params[1] == 'dn':
-                    dnattrs = True
-                else:
-                    rule = params[1]
-            elif n == 3:
-                if params[1] != 'dn':
-                    raise LDAPError('invalid extensible filter')
-                dnattrs = True
-                attr = params[0]
-                rule = params[2]
-            else:
-                raise LDAPError('invalid extensible filter')
-
-            xm = ExtensibleMatch()
-            xm.setComponentByName('matchValue', MatchValue(val))
-            xm.setComponentByName('dnAttributes', DnAttributes(dnattrs))
-            if attr:
-                xm.setComponentByName('type', Type(attr))
-            if rule:
-                xm.setComponentByName('matchingRule', MatchingRule(rule))
-            fil.setComponentByName('extensibleMatch', xm)
-        elif val.strip() == '*':
-            fil.setComponentByName('present', Present(attr))
-        elif '*' in val:
-            subf = SubstringFilter()
-            subf.setComponentByName('type', AttributeDescription(attr.strip()))
-            subs = Substrings()
-            sublist = val.split('*')
-            p = 0
-            i = 0
-            if sublist[0] != '':
-                # do initial substring
-                c = Substring()
-                c.setComponentByName('initial', Initial(sublist[0]))
-                subs.setComponentByPosition(p, c)
-                i += 1
-                p += 1
-            else:
-                i += 1
-            # do middle substrings
-            while i < len(sublist)-1:
-                if sublist[i] != '':
-                    c = Substring()
-                    c.setComponentByName('any', Any(sublist[i]))
-                    subs.setComponentByPosition(p, c)
-                    p += 1
-                i += 1
-            if sublist[i] != '':
-                # do final substring
-                c = Substring()
-                c.setComponentByName('final', Final(sublist[i]))
-                subs.setComponentByPosition(p, c)
-            subf.setComponentByName('substrings', subs)
-            fil.setComponentByName('substrings', subf)
-        else:
-            ava = EqualityMatch()
-            ava.setComponentByName('attributeDesc', AttributeDescription(attr.strip()))
-            ava.setComponentByName('assertionValue', AttributeValue(val))
-            fil.setComponentByName('equalityMatch', ava)
+        fil.setComponentByName(*_parse_ava(chunk))
     return fil
 
 
@@ -182,3 +84,105 @@ def _parse_set(filter_str, cls):
         filter_str = filter_str[end:]
         i += 1
     return fset
+
+
+def _parse_ava(chunk):
+    try:
+        attr, val = chunk.split('=', 1)
+    except ValueError:
+        raise LDAPError('Invalid filter - missing =')
+    if attr[-1] == '>':
+        ava = GreaterOrEqual()
+        ava.setComponentByName('attributeDesc', AttributeDescription(attr[0:-1].strip()))
+        ava.setComponentByName('assertionValue', AssertionValue(val))
+        return 'greaterOrEqual', ava
+    elif attr[-1] == '<':
+        ava = LessOrEqual()
+        ava.setComponentByName('attributeDesc', AttributeDescription(attr[0:-1].strip()))
+        ava.setComponentByName('assertionValue', AssertionValue(val))
+        return 'lessOrEqual', ava
+    elif attr[-1] == '~':
+        ava = ApproxMatch()
+        ava.setComponentByName('attributeDesc', AttributeDescription(attr[0:-1].strip()))
+        ava.setComponentByName('assertionValue', AssertionValue(val))
+        return 'approxMatch', ava
+    elif attr[-1] == ':':
+        # 1
+        # attr:=value
+        # 2
+        # attr:dn:=value
+        # attr:rule:=value
+        # :rule:=value
+        # 3
+        # attr:dn:rule:=value
+        # :dn:rule:=value
+
+        params = attr[0:-1].split(':')
+        n = len(params)
+
+        dnattrs = False
+        rule = None
+
+        if n == 1:
+            attr = params[0]
+        elif n == 2:
+            attr = params[0]
+            if params[1] == 'dn':
+                dnattrs = True
+            else:
+                rule = params[1]
+        elif n == 3:
+            if params[1] != 'dn':
+                raise LDAPError('invalid extensible filter')
+            dnattrs = True
+            attr = params[0]
+            rule = params[2]
+        else:
+            raise LDAPError('invalid extensible filter')
+
+        xm = ExtensibleMatch()
+        xm.setComponentByName('matchValue', MatchValue(val))
+        xm.setComponentByName('dnAttributes', DnAttributes(dnattrs))
+        if attr:
+            xm.setComponentByName('type', Type(attr))
+        if rule:
+            xm.setComponentByName('matchingRule', MatchingRule(rule))
+        return 'extensibleMatch', xm
+    elif val.strip() == '*':
+        return 'present', Present(attr)
+    elif '*' in val:
+        subf = SubstringFilter()
+        subf.setComponentByName('type', AttributeDescription(attr.strip()))
+        subs = Substrings()
+        sublist = val.split('*')
+        p = 0
+        i = 0
+        if sublist[0] != '':
+            # do initial substring
+            c = Substring()
+            c.setComponentByName('initial', Initial(sublist[0]))
+            subs.setComponentByPosition(p, c)
+            i += 1
+            p += 1
+        else:
+            i += 1
+        # do middle substrings
+        while i < len(sublist)-1:
+            if sublist[i] != '':
+                c = Substring()
+                c.setComponentByName('any', Any(sublist[i]))
+                subs.setComponentByPosition(p, c)
+                p += 1
+            i += 1
+        if sublist[i] != '':
+            # do final substring
+            c = Substring()
+            c.setComponentByName('final', Final(sublist[i]))
+            subs.setComponentByPosition(p, c)
+        subf.setComponentByName('substrings', subs)
+        return 'substrings', subf
+    else:
+        ava = EqualityMatch()
+        ava.setComponentByName('attributeDesc', AttributeDescription(attr.strip()))
+        ava.setComponentByName('assertionValue', AttributeValue(val))
+        return 'equalityMatch', ava
