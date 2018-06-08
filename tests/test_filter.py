04395301dac8cb2d5e22f9dba14c109c5b65563c
Alex Shafer <ashafer01@gmail.com>
2018-06-07 18:57:59 -0700

    improve filter testing

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -43,6 +43,22 @@ class TestFilter(unittest.TestCase):
         '(::::::=foo)',
     ]
 
+    good_standard_and_simple_filters = [
+        ('(abc=foo)', '(abc=foo)'),
+        ('(&(abc=foo)(def=bar))', '(abc=foo) AND (def=bar)'),
+        ('(&(abc=foo)(def=bar)(ghi=jkl))', '(abc=foo) AND (def=bar) AND (ghi=jkl)'),
+        ('(|(abc=foo)(def=bar)(ghi=jkl))', '(abc=foo) OR (def=bar) OR (ghi=jkl)'),
+        ('(&(abc=foo)(|(def=bar)(ghi=jkl)))', '(abc=foo) AND ((def=bar) OR (ghi=jkl))'),
+        ('(&(abc=foo)(|(def=bar)(ghi=jkl))(xyz=abc))', '(abc=foo) AND ((def=bar) OR (ghi=jkl)) AND (xyz=abc)'),
+        ('(|(abc=foo)(&(def=bar)(ghi=jkl)))', '(abc=foo) OR (def=bar) AND (ghi=jkl)'),
+        ('(|(abc=foo)(&(def=bar)(ghi=jkl)))', '(abc=foo) OR ((def=bar) AND (ghi=jkl))'),
+        ('(!(abc=foo))', 'NOT (abc=foo)'),
+        ('(&(!(abc=foo))(!(def=bar)))', 'NOT (abc=foo) AND NOT (def=bar)'),
+        ('(foo=abc AND def)', '(foo=abc AND def)'),
+        ('(&(foo=abc AND def)(bar=xyz AND abc))', '(foo=abc AND def) AND (bar=xyz AND abc)'),
+        ('(&(foo=abc OR def)(bar=xyz OR abc))', '(foo=abc OR def) AND (bar=xyz OR abc)'),
+    ]
+
     def test_parse_standard_filter(self):
         """Exercise filter parsing"""
         for f in self.good_filters:
@@ -61,34 +77,41 @@ class TestFilter(unittest.TestCase):
             f_obj = filter.parse_standard_filter(f)
             self.assertEqual(f, filter.rfc4511_filter_to_rfc4515_string(f_obj))
 
+    def test_standard_with_unified(self):
+        """Ensure all the standard test filters produce equivalent results with unified parser"""
+        for standard in self.good_filters:
+            f_obj = filter.parse(standard)
+            self.assertEqual(standard, filter.rfc4511_filter_to_rfc4515_string(f_obj),
+                             msg="Standard test filter failed with unified parser!")
+
+    def test_simple_with_unified(self):
+        """Ensure all the simple test filters produce equivalent results with unified parser"""
+        for standard, simple in self.good_standard_and_simple_filters:
+            f_obj = filter.parse(simple)
+            self.assertEqual(standard, filter.rfc4511_filter_to_rfc4515_string(f_obj),
+                             msg="Simple test filter failed with unified parser!")
+
     def test_parse_unified_filter(self):
         """Ensure parse() produces equivalent rfc4511.Filter to parse_standard_filter()"""
         tests = [
-            ('(abc=foo)', '(abc=foo)'),
-            ('(&(abc=foo)(def=bar))', '(abc=foo) AND (def=bar)'),
-            ('(&(abc=foo)(def=bar)(ghi=jkl))', '(abc=foo) AND (def=bar) AND (ghi=jkl)'),
-            ('(|(abc=foo)(def=bar)(ghi=jkl))', '(abc=foo) OR (def=bar) OR (ghi=jkl)'),
-            ('(&(abc=foo)(|(def=bar)(ghi=jkl)))', '(abc=foo) AND ((def=bar) OR (ghi=jkl))'),
-            ('(&(abc=foo)(|(def=bar)(ghi=jkl))(xyz=abc))', '(abc=foo) AND ((def=bar) OR (ghi=jkl)) AND (xyz=abc)'),
-            ('(|(abc=foo)(&(def=bar)(ghi=jkl)))', '(abc=foo) OR (def=bar) AND (ghi=jkl)'),
-            ('(|(abc=foo)(&(def=bar)(ghi=jkl)))', '(abc=foo) OR ((def=bar) AND (ghi=jkl))'),
-            ('(!(abc=foo))', 'NOT (abc=foo)'),
-            ('(&(!(abc=foo))(!(def=bar)))', 'NOT (abc=foo) AND NOT (def=bar)'),
-            ('(foo=abc AND def)', '(foo=abc AND def)'),
-            ('(&(foo=abc AND def)(bar=xyz AND abc))', '(foo=abc AND def) AND (bar=xyz AND abc)'),
-            ('(&(foo=abc OR def)(bar=xyz OR abc))', '(foo=abc OR def) AND (bar=xyz OR abc)'),
             ('(&(&(foo=abc)(bar=def))(foo=ghi)(bar=jkl))', '(&(foo=abc)(bar=def)) AND (foo=ghi) AND (bar=jkl)'),
             ('(|(&(foo=abc)(|(bar=def)(foo=ghi))(bar=jkl))(foo=mno))',
              '((foo=abc) AND ((bar=def) OR (foo=ghi)) AND (bar=jkl)) OR (foo=mno)'),
             ('(!(foo=abc NOT def))', 'NOT (foo=abc NOT def)'),
         ]
-        for standard, simple in tests:
-            f_obj = filter.parse(simple)
+        for standard, unified in tests:
+            f_obj = filter.parse(unified)
             self.assertEqual(standard, filter.rfc4511_filter_to_rfc4515_string(f_obj),
-                             msg="Orig simple filter: {0}".format(simple))
+                             msg="Orig unified filter: {0}".format(unified))
 
     def test_parse_simple_filter(self):
-        """Ensure parse_simple_filter only supports simple filters"""
+        for standard, simple in self.good_standard_and_simple_filters:
+            f_obj = filter.parse(simple)
+            self.assertEqual(standard, filter.rfc4511_filter_to_rfc4515_string(f_obj),
+                             msg="Simple test filter did not produce consistent results!")
+
+    def test_parse_simple_filter_simple_only(self):
+        """Ensure parse_simple_filter() only supports simple filters"""
         unified = '(&(foo=abc)(bar=def)) OR (&(foo=def)(bar=abc))'
         standard = '(&(foo=abc)(bar=def))'
         simple = '(foo=abc) AND (bar=def)'
