7142002f9ad4d493cb69ba144082f63e3077877a
Alex Shafer <ashafer01@gmail.com>
2019-05-30 00:21:09 -0600

    travis updates

--- scripts/build_pyasn1.sh
+++ scripts/build_pyasn1.sh
@@ -2,8 +2,7 @@
 
 echo "Building internal pyasn1"
 
-pushd "$( dirname "${BASH_SOURCE[0]}" )"
-pushd ..
+pushd "$(git rev-parse --show-toplevel)"
 
 rm -rf laurelin/ldap/pyasn1
 pushd modules/pyasn1
@@ -20,8 +19,14 @@ sed -e 's/^/# /' LICENSE.rst | sed -e 's/ $//' > pyasn1/__init__.py
 cat _tmp_init >> pyasn1/__init__.py
 
 # replace imports
-find . -name '*.py' -exec sed -i '' -E -e 's/^from pyasn1(\.[^ ]+)? import (.+)$/from laurelin.ldap.pyasn1\1 import \2/g' {} \;
-find . -name '*.py' -exec sed -i '' -E -e 's/^import pyasn1(\.[^ ]+)?$/import laurelin.ldap.pyasn1\1/g' {} \;
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    sed_flags="-i.gdbsd -E"
+else
+    sed_flags="-i.gdbsd -r"
+fi
+find . -name '*.py' -exec sed $sed_flags -e 's/^from pyasn1(\.[^ ]+)? import (.+)$/from laurelin.ldap.pyasn1\1 import \2/g' {} \;
+find . -name '*.py' -exec sed $sed_flags -e 's/^import pyasn1(\.[^ ]+)?$/import laurelin.ldap.pyasn1\1/g' {} \;
+find . -name '*.gdbsd' -delete
 
 # build patched sdist
 python3 setup.py sdist
@@ -30,7 +35,12 @@ mv _tmp_init pyasn1/__init__.py
 # install the sdist locally
 rm -rf build
 mkdir build
-pip3 install -t build dist/pyasn1-*.tar.gz
+if which pip3; then
+    pip="pip3"
+else
+    pip="pip"
+fi
+$pip install -t build dist/pyasn1-*.tar.gz
 
 # move the installed package into laurelin.ldap
 mv build/pyasn1 ../../laurelin/ldap
@@ -39,6 +49,5 @@ mv build/pyasn1 ../../laurelin/ldap
 rm -rf build dist *.egg-info
 mv orig_MANIFSET.in MANIFEST.in
 git reset --hard  # un-patch the submodule
-popd
 popd
 popd
